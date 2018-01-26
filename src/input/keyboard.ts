f2cd55e58d195c407a4d9129b288d05b2f8ba8cc
Krakarn <linus.engelbrektsson@gmail.com>
2018-01-26 15:09:19 +0100

    Added rudimentary keyboard support.

--- src/input/keyboard.ts
+++ src/input/keyboard.ts
@@ -30,3 +30,15 @@ export const key = (keyCode: number): IKeyInput => {
     state$,
   };
 };
+
+export const keys = {
+  left: 37,
+  up: 38,
+  right: 39,
+  down: 40,
+
+  a: 65,
+  d: 68,
+  s: 83,
+  w: 87,
+};
--- src/player.ts
+++ src/player.ts
@@ -3,6 +3,7 @@ import * as Rx from 'rxjs';
 import { IEntity } from './entity';
 import { create$, game$ } from './game';
 import { gamepad } from './input/gamepad';
+import { key, keys } from './input/keyboard';
 import { move } from './physics/move';
 import { IPoint } from './physics/point';
 import { position } from './physics/position';
@@ -17,8 +18,56 @@ const gamepad$ = gamepad(Rx.Observable.of({
 const leftStick$ = gamepad$.switchMap(gamepad => gamepad.sticks[0]);
 const rightStick$ = gamepad$.switchMap(gamepad => gamepad.sticks[1]);
 
-const velocity$: Rx.Observable<IPoint> = leftStick$
+const keyRight = key(keys.right);
+const keyUp = key(keys.up);
+const keyLeft = key(keys.left);
+const keyDown = key(keys.down);
+
+const keyD = key(keys.d);
+const keyW = key(keys.w);
+const keyA = key(keys.a);
+const keyS = key(keys.s);
+
+const velocityFromGamepad$: Rx.Observable<IPoint> = leftStick$
   .map(({x, y}) => ({x: x * speed, y: y * speed}))
+;
+
+const velocityXFromKeyboard$ = Rx.Observable
+  .merge(
+    keyD.down$.map(_ => 1),
+    keyD.up$.map(_ => -1),
+    keyA.down$.map(_ => -1),
+    keyA.up$.map(_ => 1),
+  )
+  .scan((sum, vx) => sum + vx, 0)
+  .map(vx => vx * speed)
+;
+
+const velocityYFromKeyboard$ = Rx.Observable
+  .merge(
+    keyS.down$.map(_ => 1),
+    keyS.up$.map(_ => -1),
+    keyW.down$.map(_ => -1),
+    keyW.up$.map(_ => 1),
+  )
+  .scan((sum, vy) => sum + vy, 0)
+  .map(vy => vy * speed)
+;
+
+const velocityFromKeyboard$: Rx.Observable<IPoint> = Rx.Observable
+  .combineLatest(
+    velocityXFromKeyboard$,
+    velocityYFromKeyboard$,
+    (x, y) => ({x, y}),
+  )
+  .startWith({x: 0, y: 0})
+;
+
+const velocity$: Rx.Observable<IPoint> = Rx.Observable
+  .merge(
+    velocityFromKeyboard$,
+    velocityFromGamepad$,
+  )
 ;
 
 const position$: Rx.Observable<IPoint> = position(velocity$);
@@ -56,8 +105,45 @@ const spawnShot = (
   ;
 };
 
-const shootDirection$ = rightStick$
-  .filter(({x, y}) => x !== 0 && y !== 0)
+const shootPointFromGamepad$ = rightStick$;
+
+const shootXFromKeyboard$ = Rx.Observable
+  .merge(
+    keyRight.down$.map(_ => 1),
+    keyRight.up$.map(_ => -1),
+    keyLeft.down$.map(_ => -1),
+    keyLeft.up$.map(_ => 1),
+  )
+  .scan((sum, sx) => sum + sx, 0)
+  .map(sx => sx * speed)
+;
+
+const shootYFromKeyboard$ = Rx.Observable
+  .merge(
+    keyDown.down$.map(_ => 1),
+    keyDown.up$.map(_ => -1),
+    keyUp.down$.map(_ => -1),
+    keyUp.up$.map(_ => 1),
+  )
+  .scan((sum, sy) => sum + sy, 0)
+  .map(sy => sy * speed)
+;
+
+const shootPointFromKeyboard$: Rx.Observable<IPoint> = Rx.Observable
+  .combineLatest(
+    shootXFromKeyboard$,
+    shootYFromKeyboard$,
+    (x, y) => ({x, y}),
+  )
+  .startWith({x: 0, y: 0})
+;
+
+const shootDirection$ = Rx.Observable
+  .merge(
+    shootPointFromGamepad$,
+    shootPointFromKeyboard$,
+  )
+  .filter(({x, y}) => x !== 0 || y !== 0)
   .map(({x, y}) => Math.atan2(y, x))
 ;
 
